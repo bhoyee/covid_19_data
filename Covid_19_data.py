66b99545cbf22e14c1eb0cdcdb6b98501b6363a5
unknown <s.adeboye@outlook.com>
2023-05-19 22:14:36 +0100

    load data to Postgres

--- Covid_19_data.py
+++ Covid_19_data.py
@@ -24,3 +24,49 @@ conn = psycopg2.connect(
     password=os.environ['MY_DB_PASS'],
 )
 cur = conn.cursor()
+
+# Create the COVID-19 data table if it doesn't exist
+cur.execute('''
+    CREATE TABLE IF NOT EXISTS covid_19_data (
+        serial_number INT,
+        observation_date DATE,
+        state VARCHAR(255),
+        region VARCHAR(255),
+        last_updated VARCHAR(255),
+        confirmed VARCHAR(255),
+        deaths INT,
+        recovered INT
+    )
+''')
+conn.commit()
+
+# Load the data from the CSV file into the COVID-19 data table
+csv_reader = csv.reader(csv_data.split('\n'), delimiter=',')
+next(csv_reader)  # Skip the header row
+for row in csv_reader:
+    # Check that the row is not empty
+    if row:
+        # Check that the row contains the expected number of columns
+        if len(row) == 8:
+            cur.execute('''
+                INSERT INTO covid_19_data (
+                    serial_number,
+                    observation_date,
+                    state,
+                    region,
+                    last_updated,
+                    confirmed,
+                    deaths,
+                    recovered
+                ) VALUES (%s, TO_DATE(%s, 'MM/DD/YYYY'), %s, %s, %s, %s, %s, %s)
+            ''', row)
+        else:
+            print(f'Warning: row {row} does not contain the expected number of columns')
+    else:
+        print('Warning: empty row')
+conn.commit()
+print('Data loaded successfully!')
+
+# Close the database connection
+cur.close()
+conn.close()
